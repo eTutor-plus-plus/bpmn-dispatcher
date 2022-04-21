00debda3293d6229351e795f6d1e21a90f289418
Dominik <dschuhmann96@gmail.com>
2022-04-21 21:19:23 +0200

    Initial commit

---
+++ src/main/java/etutor/bpmndispatcher/BpmnDispatcherApplication.java
@@ -0,0 +1,13 @@
+package etutor.bpmndispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmnDispatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmnDispatcherApplication.class, args);
+    }
+
+}

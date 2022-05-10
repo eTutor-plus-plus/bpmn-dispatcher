2a9ef0de40b38c1e5e44b5bee7b6375a7f97b7b7
Dominik <dschuhmann96@gmail.com>
2022-05-10 12:32:46 +0200

    wip

--- src/main/java/etutor/bpmndispatcher/rest/controller/TestController.java
+++ src/main/java/etutor/bpmndispatcher/rest/controller/TestController.java
@@ -3,14 +3,11 @@ package etutor.bpmndispatcher.rest.controller;
 import etutor.bpmndispatcher.rest.service.bpmnValidationModul.BpmnDeploymentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/test")
@@ -24,25 +21,28 @@ public class TestController {
 
 
     @PostMapping(path = "")
-    public String deployBPMN(@RequestBody String bpmnXml) {
-        try {
-            String result = bpmnDeploymentService.deployNewBpmnWithString(bpmnXml);
-            if (result.contains("ParseException")) {
-                return result;
-            } else {
-                JSONObject obj = new JSONObject(result);
-                String id = obj.getString("id");
-                logger.info("ID:+++++++" + id);
-                String definitionID = obj.getJSONObject("deployedProcessDefinitions").keys().next().toString();
-                logger.warn(definitionID);
-                logger.info("---Deployed!---");
-                return definitionID;
-            }
-        } catch (IOException e) {
-            logger.warn(e.getMessage());
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-        return "Deployment failed";
+    public ResponseEntity<String> deployBPMN(@RequestBody String bpmnXml) {
+        if (bpmnXml == null || bpmnXml.isBlank()) return ResponseEntity.status(500).body("no Bpmn");
+        logger.info(bpmnXml);
+
+//        try {
+//            String result = bpmnDeploymentService.deployNewBpmnWithString(bpmnXml);
+//            if (result.contains("ParseException")) {
+//                return result;
+//            } else {
+//                JSONObject obj = new JSONObject(result);
+//                String id = obj.getString("id");
+//                logger.info("ID:+++++++" + id);
+//                String definitionID = obj.getJSONObject("deployedProcessDefinitions").keys().next().toString();
+//                logger.warn(definitionID);
+//                logger.info("---Deployed!---");
+//                return definitionID;
+//            }
+//        } catch (IOException e) {
+//            logger.warn(e.getMessage());
+//        } catch (JSONException e) {
+//            throw new RuntimeException(e);
+//        }
+        return ResponseEntity.ok("Deployment");
     }
 }
